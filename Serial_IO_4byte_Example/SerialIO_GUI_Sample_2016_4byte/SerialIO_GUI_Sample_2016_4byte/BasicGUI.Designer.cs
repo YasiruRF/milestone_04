34042ed19f15d3a7f1caa30c53aef92177461054
Yasiru Fernando <dyrfernando@gmail.com>
2025-10-13 16:05:51 +0530

    first commit

---
+++ Serial_IO_4byte_Example/SerialIO_GUI_Sample_2016_4byte/SerialIO_GUI_Sample_2016_4byte/BasicGUI.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace SerialGUISample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serial = new System.IO.Ports.SerialPort(this.components);
+            this.getIOtimer = new System.Windows.Forms.Timer(this.components);
+            this.InputBox1 = new System.Windows.Forms.TextBox();
+            this.OutputBox1 = new System.Windows.Forms.NumericUpDown();
+            this.Send1 = new System.Windows.Forms.Button();
+            this.Send2 = new System.Windows.Forms.Button();
+            this.Get1 = new System.Windows.Forms.Button();
+            this.Get2 = new System.Windows.Forms.Button();
+            this.statusBox = new System.Windows.Forms.TextBox();
+            this.InputBox2 = new System.Windows.Forms.TextBox();
+            this.OutputBox2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serial
+            // 
+            this.serial.PortName = "COM12";
+            // 
+            // getIOtimer
+            // 
+            this.getIOtimer.Enabled = true;
+            this.getIOtimer.Interval = 10;
+            this.getIOtimer.Tick += new System.EventHandler(this.getIOtimer_Tick);
+            // 
+            // InputBox1
+            // 
+            this.InputBox1.Location = new System.Drawing.Point(53, 119);
+            this.InputBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.InputBox1.Name = "InputBox1";
+            this.InputBox1.Size = new System.Drawing.Size(167, 22);
+            this.InputBox1.TabIndex = 0;
+            this.InputBox1.Text = "0";
+            // 
+            // OutputBox1
+            // 
+            this.OutputBox1.DecimalPlaces = 1;
+            this.OutputBox1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.OutputBox1.Location = new System.Drawing.Point(53, 84);
+            this.OutputBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.OutputBox1.Name = "OutputBox1";
+            this.OutputBox1.Size = new System.Drawing.Size(167, 22);
+            this.OutputBox1.TabIndex = 3;
+            // 
+            // Send1
+            // 
+            this.Send1.Location = new System.Drawing.Point(228, 48);
+            this.Send1.Margin = new System.Windows.Forms.Padding(4);
+            this.Send1.Name = "Send1";
+            this.Send1.Size = new System.Drawing.Size(100, 28);
+            this.Send1.TabIndex = 4;
+            this.Send1.Text = "DutyCycle1";
+            this.Send1.UseVisualStyleBackColor = true;
+            this.Send1.Click += new System.EventHandler(this.Send1_Click);
+            // 
+            // Send2
+            // 
+            this.Send2.Location = new System.Drawing.Point(228, 84);
+            this.Send2.Margin = new System.Windows.Forms.Padding(4);
+            this.Send2.Name = "Send2";
+            this.Send2.Size = new System.Drawing.Size(100, 28);
+            this.Send2.TabIndex = 4;
+            this.Send2.Text = "DutyCycle2";
+            this.Send2.UseVisualStyleBackColor = true;
+            this.Send2.Click += new System.EventHandler(this.Send2_Click);
+            // 
+            // Get1
+            // 
+            this.Get1.Location = new System.Drawing.Point(227, 117);
+            this.Get1.Margin = new System.Windows.Forms.Padding(4);
+            this.Get1.Name = "Get1";
+            this.Get1.Size = new System.Drawing.Size(100, 28);
+            this.Get1.TabIndex = 4;
+            this.Get1.Text = "Input 1";
+            this.Get1.UseVisualStyleBackColor = true;
+            this.Get1.Click += new System.EventHandler(this.Get1_Click);
+            // 
+            // Get2
+            // 
+            this.Get2.Location = new System.Drawing.Point(227, 153);
+            this.Get2.Margin = new System.Windows.Forms.Padding(4);
+            this.Get2.Name = "Get2";
+            this.Get2.Size = new System.Drawing.Size(100, 28);
+            this.Get2.TabIndex = 4;
+            this.Get2.Text = "Input 2";
+            this.Get2.UseVisualStyleBackColor = true;
+            this.Get2.Click += new System.EventHandler(this.Get2_Click);
+            // 
+            // statusBox
+            // 
+            this.statusBox.Location = new System.Drawing.Point(53, 217);
+            this.statusBox.Margin = new System.Windows.Forms.Padding(4);
+            this.statusBox.Name = "statusBox";
+            this.statusBox.Size = new System.Drawing.Size(167, 22);
+            this.statusBox.TabIndex = 5;
+            // 
+            // InputBox2
+            // 
+            this.InputBox2.Location = new System.Drawing.Point(53, 153);
+            this.InputBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.InputBox2.Name = "InputBox2";
+            this.InputBox2.Size = new System.Drawing.Size(167, 22);
+            this.InputBox2.TabIndex = 0;
+            this.InputBox2.Text = "0";
+            // 
+            // OutputBox2
+            // 
+            this.OutputBox2.DecimalPlaces = 1;
+            this.OutputBox2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.OutputBox2.Location = new System.Drawing.Point(53, 48);
+            this.OutputBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.OutputBox2.Name = "OutputBox2";
+            this.OutputBox2.Size = new System.Drawing.Size(167, 22);
+            this.OutputBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 256);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.Get2);
+            this.Controls.Add(this.Get1);
+            this.Controls.Add(this.Send2);
+            this.Controls.Add(this.Send1);
+            this.Controls.Add(this.OutputBox2);
+            this.Controls.Add(this.OutputBox1);
+            this.Controls.Add(this.InputBox2);
+            this.Controls.Add(this.InputBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OutputBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer getIOtimer;
+        private System.Windows.Forms.TextBox InputBox1;
+        private System.Windows.Forms.NumericUpDown OutputBox1;
+        private System.IO.Ports.SerialPort serial;
+        private System.Windows.Forms.Button Send1;
+        private System.Windows.Forms.Button Send2;
+        private System.Windows.Forms.Button Get1;
+        private System.Windows.Forms.Button Get2;
+        private System.Windows.Forms.TextBox statusBox;
+        private System.Windows.Forms.TextBox InputBox2;
+        private System.Windows.Forms.NumericUpDown OutputBox2;
+    }
+}
+
---
+++ Serial_IO_4byte_Example/SerialIO_GUI_Sample_2016_4byte/SerialIO_GUI_Sample_2016_4byte/BasicGUI.cs
@@ -0,0 +1,178 @@
+﻿// Curtin University
+// Mechatronics Engineering
+// Serial I/O Card - Sample GUI Code
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SerialGUISample
+{    
+
+    public partial class Form1 : Form
+    {
+        // Declare variables to store inputs and outputs.
+        bool runSerial = true;
+        bool byteRead = false;
+        int Input1 = 0;
+        int Input2 = 0;
+
+        byte[] Outputs = new byte[4];
+        byte[] Inputs = new byte[4];
+
+        const byte START = 255;
+        const byte ZERO = 0;
+        
+
+        public Form1()
+        {
+            // Initialize required for form controls.
+            InitializeComponent();
+
+            // Establish connection with serial
+            if (runSerial == true)
+            {
+                if (!serial.IsOpen)                                  // Check if the serial has been connected.
+                {
+                    try
+                    {
+                        serial.Open();                               //Try to connect to the serial.
+                    }
+                    catch
+                    {
+                        statusBox.Enabled = false;
+                        statusBox.Text ="ERROR: Failed to connect.";     //If the serial does not connect return an error.
+                    }
+                }
+            }
+        }
+
+        // Send a four byte message to the Arduino via serial.
+        private void sendIO(byte PORT, byte DATA)
+        {
+            Outputs[0] = START;    //Set the first byte to the start value that indicates the beginning of the message.
+            Outputs[1] = PORT;     //Set the second byte to represent the port where, Input 1 = 0, Input 2 = 1, Output 1 = 2 & Output 2 = 3. This could be enumerated to make writing code simpler... (see Arduino driver)
+            Outputs[2] = DATA;  //Set the third byte to the value to be assigned to the port. This is only necessary for outputs, however it is best to assign a consistent value such as 0 for input ports.
+            Outputs[3] = (byte)(START + PORT + DATA); //Calculate the checksum byte, the same calculation is performed on the Arduino side to confirm the message was received correctly.
+
+            if (serial.IsOpen)
+            {
+                serial.Write(Outputs, 0, 4);         //Send all four bytes to the IO card.                      
+            }
+        }
+
+        private byte Maptobyte(decimal value) //to map the value from -15 to 15 into a byte value from 0 to 255.
+        {
+            double scaled = ((double)value * 8.5) + 127.5;
+            return (byte)Math.Round(scaled);
+        }
+
+        private string ToBinaryStrong(byte value) //to convert a byte value to a binary string for display.
+        {
+            return Convert.ToString(value, 2).PadLeft(8, '0');
+        }
+
+        // Map 0-100% to 0-255
+        private byte MapDutyCycle(decimal value)
+        {
+            double scaled = ((double)value * 2.55);
+            return(byte)Math.Round(scaled);
+
+           
+        }
+
+        // Alternative method to map -100% to +100% to -15 to +15 then to 0-255
+        //private byte MapDutyCycle(decimal percent)
+        //{
+        //    // Map 0–100% -> -15 to +15
+        //    double mappedVal = -15 + (double)percent / 100.0 * 30.0;
+
+        //    // Map -15..+15 -> 0–255
+        //    double scaled = ((mappedVal + 15) / 30.0) * 255.0;
+
+        //    return (byte)Math.Round(scaled);
+        //}
+
+
+
+
+        private void Send1_Click(object sender, EventArgs e) //Press the button to send the value to Output 1, Arduino Port A.
+        {
+            byte mappedDutyCycle = MapDutyCycle(OutputBox1.Value);
+            sendIO(2, mappedDutyCycle);
+        }
+
+        private void Send2_Click(object sender, EventArgs e) //Press the button to send the value to Output 2, Arduino Port C.
+        {
+            byte mappedDutyCycle = MapDutyCycle(OutputBox2.Value);
+            sendIO(3, mappedDutyCycle);
+        }
+
+        private void Get1_Click(object sender, EventArgs e) //Press the button to request value from Input 1, Arduino Port F.
+        {
+            sendIO(0, ZERO);  // The value 0 indicates Input 1, ZERO just maintains a fixed value for the discarded data in order to maintain a consistent package format.
+        }
+
+        private void Get2_Click(object sender, EventArgs e) //Press the button to request value from Input 1, Arduino Port K.
+        {
+            sendIO(1, ZERO);  // The value 1 indicates Input 2, ZERO maintains a consistent value for the message output.
+        }
+
+        private void getIOtimer_Tick(object sender, EventArgs e) //It is best to continuously check for incoming data as handling the buffer or waiting for event is not practical in C#.
+        {
+            if (serial.IsOpen) //Check that a serial connection exists.
+            {
+                if (serial.BytesToRead >= 4) //Check that the buffer contains a full four byte package.
+                {
+                    //statusBox.Text = "Incoming"; // A status box can be used for debugging code.
+                    Inputs[0] = (byte)serial.ReadByte(); //Read the first byte of the package.
+
+                    if (Inputs[0] == START) //Check that the first byte is in fact the start byte.
+                    {
+                        //statusBox.Text = "Start Accepted";
+
+                        //Read the rest of the package.
+                        Inputs[1] = (byte)serial.ReadByte();
+                        Inputs[2] = (byte)serial.ReadByte();
+                        Inputs[3] = (byte)serial.ReadByte();
+
+                        //Calculate the checksum.
+                        byte checkSum = (byte)(Inputs[0] + Inputs[1] + Inputs[2]);
+
+                        //Check that the calculated check sum matches the checksum sent with the message.
+                        if (Inputs[3] == checkSum)
+                        {
+                            //statusBox.Text = "CheckSum Accepted";
+
+                            //Check which port the incoming data is associated with.
+                            switch (Inputs[1])
+                            {
+                                case 0: //Save the data to a variable and place in the textbox.
+                                    //statusBox.Text = "Input1";
+                                    Input1 = Inputs[2];
+                                    InputBox1.Text = Input1.ToString();
+                                    break;
+                                case 1: //Save the data to a variable and place in the textbox. 
+                                    //statusBox.Text = "Input2";
+                                    Input2 = Inputs[2];
+                                    InputBox2.Text = Input2.ToString();
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
